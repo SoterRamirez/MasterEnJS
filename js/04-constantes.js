ab4995d0a311a9d20763ed697df63b04aabc6ca2
SoterRamirez <soterrc@gmail.com>
2019-02-09 21:35:47 -0600

    Sec 6 Class 21 Constantes

---
+++ js/04-constantes.js
@@ -0,0 +1,9 @@
+'use strict'
+//Constante
+//Contenendor de datos (como una variable), solo que no va a poder ser modificado
+
+var web = "https://google.com.mx";
+const ip = "192.168.1.254"; //Valor no puede ser modificado.
+
+web = "google.es";
+console.log(web, ip);
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 ### Sec6 Clas18 Variables y Comentarios
 ### Sec 6 Clas 19 Modo Estricto
 ### Sec 6 Clas 20 Let y Var
+### Sec 6 Class 21 Constantes
