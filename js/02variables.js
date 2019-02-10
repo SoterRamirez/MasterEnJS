fe30dd7306f0daa4e0ee72fe328a64ed9b84c1f3
SoterRamirez <soterrc@gmail.com>
2019-02-09 21:08:59 -0600

    Sec6 Clas18 Variables y Comentarios

---
+++ js/02variables.js
@@ -0,0 +1,11 @@
+//Variable
+//Contenedor de información.
+
+var pais = "Mexico";
+var continente = "Latinoamerica";
+var antiguedad = "2099";
+var pais_y_continente = pais+' '+ continente;
+pais = "España";
+continente = "Europa";
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # Curso de Js
+
+
+### Sec6 Clas18 Variables y Comentarios
