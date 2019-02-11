2ef8ecd7214ab19428e973d76562554ce7e3686b
SoterRamirez <soterrc@gmail.com>
2019-02-10 19:19:34 -0600

    Sec 7 Class 22 Operadores y tipos de datos / Se agrega img

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,24 @@
+'use strict'
+//Operaddores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+//alert ("El resultado de la operación es: "+operacion);
+
+
+//Tipo de datos
+var numero_entero = 44;
+var cadena_texto = "hola 'que' tal";
+var verdader_falso = true;
+
+var numero_falso = "33"
+Number();
+
+alert (Number(numero_falso)+7);
+
+//parseInt te convierte un algo a un numero entero.
+
+//parseFloat a un numero con decimales si tiene que tener decimales.
+
+//Number a un numero genérico.
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@
 ### Sec 6 Clas 19 Modo Estricto
 ### Sec 6 Clas 20 Let y Var
 ### Sec 6 Class 21 Constantes
+### Sec 7 Class 22 Operadores y tipos de datos
