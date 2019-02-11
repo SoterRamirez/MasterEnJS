6dd96afd08aa28222dd4f23d5a2b441062c168a9
SoterRamirez <soterrc@gmail.com>
2019-02-10 20:01:06 -0600

    Secc 8 Class 24 Condicional if

---
+++ js/06-condicionales.js
@@ -0,0 +1,24 @@
+'use strict'
+//Uso de la condicional if
+//Una estructura de control o de instrucciones para permitir comparar algo
+//Si A es igual a B entoces haz algo
+
+
+var edad1 = 30;
+var edad2 = 120;
+
+/*
+Operadores relacionales
+    Mayor: >
+    Menor: <
+    Mayor o igual: >=
+    Menor o igual: <=
+    Distinto: !=
+ */
+
+//Si pasa esto
+if (edad1 > edad2) {
+    //Ejecuta esto
+    alert("Edad uno es mayor que edad 2");
+}//Si no cumple ejecuta esto
+else alert ("La edad uno no es inferior a la edad 2");
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Curso de Js
 
-
+## Sección 6 Variables
 ### Sec6 Clas18 Variables y Comentarios
 ### Sec 6 Clas 19 Modo Estricto
 ### Sec 6 Clas 20 Let y Var
 ### Sec 6 Class 21 Constantes
+## Sección 7 Tipos de datos Y Operadores
 ### Sec 7 Class 22 Operadores y tipos de datos
 ### Sec 7 Class 23 Detectar el tipo de datos con Typeof
+## Sección 8 Estructura de control -Condicionales
+### Secc 8 Class 24 Condicional if
